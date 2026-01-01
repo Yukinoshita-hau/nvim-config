02af64cd08264a4d70692fec3cf75ebc3e387d67
RASHID_THE_FUCKER <Rashid16062006mor@yandex.ru>
2026-01-01 17:19:00 +0500

    именён цвет lualine, с dashboard убраны фразы и подкоректированы шорткаты

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,23 @@ return {
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("lualine").setup()
+			require("lualine").setup({
+				options = {
+					theme = 'gruvbox-material',
+					icons_enabled = true,
+					component_separators = { left = '', right = '' },
+					section_separators = { left = '', right = '' },
+				},
+
+				sections = {
+					lualine_a = { 'mode' },
+					lualine_b = { 'branch', 'diff', 'diagnostics' },
+					lualine_c = { 'filename' },
+					lualine_x = { 'encoding', 'fileformat', 'filetype' },
+					lualine_y = { 'progress' },
+					lualine_z = { 'location' }
+				},
+			})
 		end
 	}
 }
