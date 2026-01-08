04b03f60ad4749835590c6dd9b94d31ca6f08078
RASHID_THE_FUCKER <Rashid16062006mor@yandex.ru>
2026-01-08 14:21:13 +0500

    улучшен lualine добавлено несколько новый полей.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -14,7 +14,22 @@ return {
 				sections = {
 					lualine_a = { 'mode' },
 					lualine_b = { 'branch', 'diff', 'diagnostics' },
-					lualine_c = { 'filename' },
+					lualine_c = {
+						{
+							'filename',
+							file_status = true,
+							path = 1,
+							shorting_target = 40,
+							symbols = {
+								modified = '[Modified]',
+								readonly = '[Readonly]',
+								unnamed = '[No Name]',
+								newfile = '[New]',
+							}
+						},
+						"hostname",
+						'filesize'
+					},
 					lualine_x = { 'encoding', 'fileformat', 'filetype' },
 					lualine_y = { 'progress' },
 					lualine_z = { 'location' }
